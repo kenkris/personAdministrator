5947b66d6654b9925e7891addb6ebaa503fb4906
Kenneth Kristensen <kennethkristensen@Kenneths-MacBook-Pro.local>
2019-03-21 22:19:40 +0100

    Add todos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
     <b-container> 
         <b-row class="text-center">
-            <b-col>1 of 3</b-col>
-            <b-col cols="8">2 of 3 (wider)</b-col>
-            <b-col>3 of 3</b-col>
+            <b-col>1 of 3 Listgroup menu</b-col>
+            <b-col cols="8">2 of 3 user list here</b-col>
+            <b-col>Card with add </b-col>
         </b-row>
     </b-container>
 </template>
